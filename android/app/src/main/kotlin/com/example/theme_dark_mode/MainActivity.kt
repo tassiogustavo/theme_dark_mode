2658985b7f5c63a999c0d5cb132b289943218471
Tassio Gustavo <tassio-v@outlook.com>
2022-06-23 21:50:25 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/com/example/theme_dark_mode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.theme_dark_mode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/theme_constants.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+const colorPrimary = Colors.deepOrangeAccent;
+const colorAccent = Colors.orange;
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: colorPrimary,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: colorAccent
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                const EdgeInsets.symmetric(horizontal: 40.0,vertical: 20.0)
+            ),
+            shape: MaterialStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0))
+            ),
+            backgroundColor: MaterialStateProperty.all<Color>(colorAccent)
+        )
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),borderSide: BorderSide.none
+        ),
+        filled: true,
+        fillColor: Colors.grey.withOpacity(0.1)
+    )
+);
+
+ThemeData darkTheme = ThemeData(
+
+  brightness: Brightness.dark,
+  accentColor: Colors.white,
+  switchTheme: SwitchThemeData(
+    trackColor: MaterialStateProperty.all<Color>(Colors.grey),
+    thumbColor: MaterialStateProperty.all<Color>(Colors.white),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),borderSide: BorderSide.none
+      ),
+      filled: true,
+      fillColor: Colors.grey.withOpacity(0.1)
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.symmetric(horizontal: 40.0,vertical: 20.0)
+          ),
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+              )
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          overlayColor: MaterialStateProperty.all<Color>(Colors.black26)
+      )
+  ),
+);
